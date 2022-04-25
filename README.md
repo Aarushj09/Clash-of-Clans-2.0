40082895979b5d2e910bc570e17af0b9e0f66dc7
Aarushj09 <68746944+Aarushj09@users.noreply.github.com>
2022-04-25 23:44:24 +0530

    Initial Game

---
+++ README.md
@@ -0,0 +1,19 @@
+# Assignment 3.1 - Clash of Clans
+
+- To run the code, enter the command **`python3 game.py`**.
+- The king or queen can be moved using **`W,A,S,D`**.
+- The king or queen attacks, normally as explained in the assignment document, by pressing **`Space`**.
+- The king attacks using Levithian Axe by pressing **`L`**.
+- The queen shoots Eagle Arrow on pressing **`L`**.
+- Archers can be spawned using **`1,2,3`**.
+- Balloons can be spawned using **`4,5,6`**.
+- Barbarians can be spawned using **`7,8,9`**.
+- Activate rage spell and heal spell using **`R`** and **`H`** respectively.
+- To exit the game in mid, press **`E`**.
+
+
+- Maximum of **`10`** barbarians, **`10`** archers and **`10`** ballons can be spawned.
+- Tower and cannon have a range of **`5`** tiles.
+- Archer has range of **`6`** tiles.
+
+- To replay a particular attack, enter the command **`python3 replay.py k.txt`**, where **`k`** is the number denoting the **`k-th`** attack.
---
+++ game.py
@@ -0,0 +1,119 @@
+from time import sleep
+
+
+from src.headers import *
+from src.utility import *
+from src.input import *
+
+os.system('clear')
+
+file = open(f"replays/{len(os.listdir('./replays')) + 1}.txt", "w")
+village = Village([[SCREEN_HEIGHT//3,0],[0,SCREEN_WIDTH//2],[2*SCREEN_HEIGHT//3,SCREEN_WIDTH-1]])
+initialise_village_1(village)
+village.level=1
+inputt = Get()
+it1 = 0
+it2 = 0
+it3 = 0
+it_archer = 0
+it_balloon = 0
+it_tower = 0
+level = 1
+
+
+while True:
+    character = input("Press 0 for King, 1 for Queen: ")
+    file.write(str(character)+'\n')
+    if(character == '0'):
+        village.troops.append(King())
+        village.character = 1
+        break
+    elif(character == '1'):
+        village.troops.append(Queen())
+        village.character = 4
+        break
+    else:
+        print("Enter valid input!!")
+
+
+while True:
+    sleep(0.001)
+    it1+=1
+    it2+=1
+    it_archer+=2
+    it_balloon+=2
+    it_tower+=1
+    if village.rage_spell==1:
+        it3+=1
+
+    for x in village.buildings:
+        if x.health <= 0:
+            village.buildings.remove(x)
+    for x in village.troops:
+        if x.health <= 0:
+            village.troops.remove(x)
+
+    village.render()
+    if(it3>100):
+        village.rage_spell = 0
+        it3 = 0
+
+
+    result = game_over(village)
+    if result!=-1:
+        if result == 0:
+            break
+        if level==1:
+            level = 2
+            village = Village([[SCREEN_HEIGHT//3,0],[0,SCREEN_WIDTH//2],[2*SCREEN_HEIGHT//3,SCREEN_WIDTH-1]])
+            initialise_village_2(village)
+            village.level=2
+            if(character == '0'):
+                village.troops.append(King())
+                village.character = 1
+                
+            elif(character == '1'):
+                village.troops.append(Queen())
+                village.character = 4
+        elif level==2:
+            level = 3
+            village = Village([[SCREEN_HEIGHT//3,0],[0,SCREEN_WIDTH//2],[2*SCREEN_HEIGHT//3,SCREEN_WIDTH-1]])
+            initialise_village_3(village)
+            village.level=3
+            if(character == '0'):
+                village.troops.append(King())
+                village.character = 1
+            elif(character == '1'):
+                village.troops.append(Queen())
+                village.character = 4
+        elif level==3:
+            break
+        # break
+
+    inp = input_to(inputt)
+    file.write(str(inp)+'\n')
+    if(inp != None):
+        handle_input(inp,village)
+
+    if(it1 >= 10/(village.rage_spell+1)):
+        village.move_attack_barbs()
+        it1 = 0
+    if(it_archer >= 10/(village.rage_spell+1)):
+        village.move_attack_archers()
+        it_archer = 0
+    if(it_balloon >= 10/(village.rage_spell+1)):
+        village.move_attack_balloons()
+        it_balloon = 0
+    if(it2 >= 10):
+        village.fire_cannons()
+        it2 = 0
+    if(it_tower>=10):
+        village.fire_towers()
+        it_tower = 0
+
+if(result == 0):
+    defeat()
+else:
+    victory()
+
+file.close()
